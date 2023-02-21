a4137e28a83c383644f7e1212e51d0122b109f1d
tinhanho <67796326+tinhanho@users.noreply.github.com>
2023-02-21 21:52:40 +0800

    Update LongestCommonPrefix.c

--- LongestCommonPrefix.c
+++ LongestCommonPrefix.c
@@ -1,22 +1,18 @@
 char * longestCommonPrefix(char ** strs, int strsSize){
     /*note we only care about the prefix */
     if(strsSize==1) return strs[0];
-    char *res = malloc(sizeof(char)*(200+1)); /* strs[i].length<=200 */
-    char *res_head = res;
     char *tmp = strs[0];
+    char *res = tmp;
     int j=0, flag=0;
     while(1){
         for(int i=0; i<=strsSize-1;i++){
-            if(strs[i][j]!=*tmp || strs[i][j]==NULL){
-                flag = 1;
-            }
+            if(strs[i][j]!=*tmp || strs[i][j]==NULL) flag = 1;
+            if(flag==1) break;
         }
         if(flag==1) break;
-        *res=*tmp;
         tmp++;
         j++;
-        res++;
     }
-    *res = '\0';
-    return res_head;
+    *tmp = '\0';
+    return res;
 }
